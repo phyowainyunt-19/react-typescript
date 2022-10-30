b88b04d8f00f41244bce7b6169b5378462c3c6a9
phyowainyunt-19 <phyowainyunt.26@gmail.com>
2022-10-30 15:06:42 +0630

    Component props added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import './App.css';
 import { Greet } from "./components"
-import Counter from './components/class/Counter';
 
 function App() {
 
   return (
     <div className='App'>
       <Greet name="PWN" isLoggedIn={false} />
-      <Counter message='The count value is : '/>
     </div>
 
   )
